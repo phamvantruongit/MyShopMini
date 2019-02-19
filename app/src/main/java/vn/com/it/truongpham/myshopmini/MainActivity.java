92de644a85cb352402a619ee4a93a73d7a22f39d
PhamTruong <phamvantruongit@gmail.com>
2019-02-19 22:37:21 +0700

    commit

--- app/src/main/java/vn/com/it/truongpham/myshopmini/MainActivity.java
+++ app/src/main/java/vn/com/it/truongpham/myshopmini/MainActivity.java
@@ -2,15 +2,23 @@ package vn.com.it.truongpham.myshopmini;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.bottomnavigation.LabelVisibilityMode;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import vn.com.it.truongpham.myshopmini.fragment.FragmentBH;
+import vn.com.it.truongpham.myshopmini.fragment.FragmentLoaiSP;
+import vn.com.it.truongpham.myshopmini.fragment.FragmentSP;
+import vn.com.it.truongpham.myshopmini.fragment.FragmentThem;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+   Fragment fragment;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -18,13 +26,21 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    fragment=new FragmentLoaiSP();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    fragment=new FragmentSP();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    fragment=new FragmentBH();
+                    loadFragment(fragment);
+                    return true;
+
+                case R.id.navigation:
+                    fragment=new FragmentThem();
+                    loadFragment(fragment);
                     return true;
             }
             return false;
@@ -36,9 +52,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        fragment=new FragmentLoaiSP();
+        loadFragment(fragment);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+    private void loadFragment(Fragment fragment) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
---
+++ app/src/main/java/vn/com/it/truongpham/myshopmini/fragment/FragmentLoaiSP.java
@@ -0,0 +1,30 @@
+package vn.com.it.truongpham.myshopmini.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vn.com.it.truongpham.myshopmini.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentLoaiSP extends Fragment {
+
+
+    public FragmentLoaiSP() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_loaisp, container, false);
+    }
+
+}
